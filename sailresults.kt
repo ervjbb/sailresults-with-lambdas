7e4fd857d1fa210eef0d9a7adaebcaafca447cde
Johan Baerring <45194960+ervjbb@users.noreply.github.com>
2026-03-05 20:44:45 +0100

    Enhance Results class with race discarding logic
    Refactor result calculation to support discarding races and update main function for testing.

--- sailresults.kt
+++ sailresults.kt
@@ -1,29 +1,47 @@
 class Results(val totalResult: List<Int>) {
 
 fun resultRefinement (
-	withDiscardedRaces: (totalResult: List<Int>) -> Int
+	withDiscardedRaces: (totalResult: List<Int>) -> Int    // this is a lambda function!
 ) {
 	val resultAfterDiscardedRaces = withDiscardedRaces(totalResult)
         println("Final result after discards: $resultAfterDiscardedRaces")
   }
 }
 
-fun main() {
-	var highestRaceScoreSoFar = 0
-	val res = listOf(12,13,7,4,3,5,6,8,15,17,9)
-	val results = Results(res)
-	val resultsWithNoDiscards = res.sum()
+fun resultCalculation(totalResults: MutableList<Int>, nmbrOfDiscards: Int) {
+        var discardedResults = arrayOf(0,0,0) // max 3 discards supported
+        val sumOfAllRaces = totalResults.sum()
+	val results = Results(totalResults)   // intitialize the class Results
+	var raceNmbr: Int
+	var worstRace: Int
+
 	results.resultRefinement { x: List<Int> ->
-		for (racePlacement in x) {
-			if (racePlacement > highestRaceScoreSoFar) {
-                            highestRaceScoreSoFar = racePlacement
-                            println("Finding new highestRaceScore: $highestRaceScoreSoFar")
+	for (i in 0..nmbrOfDiscards-1) {
+		raceNmbr = 0
+		worstRace = 0
+		for (raceResult in x) {
+			if (raceResult > discardedResults[i]) {
+			    worstRace = raceNmbr
+                            discardedResults[i] = raceResult
+                            println("Finding new highestRaceScore: $raceResult")
 		    }
-		}
-		res.sum() - highestRaceScoreSoFar
-		}
-        println("Results with no discards: $resultsWithNoDiscards")
+		    raceNmbr++
+		
+	    	}
+		totalResults[worstRace] = 0
+	}
+	sumOfAllRaces - discardedResults.sum()
+	}
+        println("Results with no discards: $sumOfAllRaces")
 }
+
+
+fun main() {
+	val totalResults = mutableListOf(12,13,7,4,3,5,6,8,15,17,9)  // this is just to test the lambdas
+	val nmbrOfDiscards = 2
+	resultCalculation( totalResults, nmbrOfDiscards)
+}
+
 
 
 
