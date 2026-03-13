9224cabb402e167f8355f5416e04b95e30485736
Johan Baerring <45194960+ervjbb@users.noreply.github.com>
2026-03-13 13:40:12 +0100

    Refactor resultCalculation and resultRefinement methods

--- sailresults.kt
+++ sailresults.kt
@@ -1,27 +1,24 @@
 class Results(val totalResult: List<Int>) {
-
-fun resultRefinement ( resultPolish: ( List<Int> ) -> Int ) {       // this is a lambda funtion
+  fun resultRefinement ( resultPolish: ( List<Int> ) -> Int ) {       // this is a lambda funtion
      val finalResult = resultPolish(totalResult)                               // it has no knowledge of discards!
      println("Final result: $finalResult")                                     
+  }
 }
  
-  }
- 
 fun resultCalculation(totalResults: MutableList<Int>, nmbrOfDiscards: Int = 0) {   // default nmbr of discards = 0
-        val sumOfAllRaces = totalResults.sum()
-	val results = Results(totalResults.sorted() )   // intitialize the class Results with a sorted list of race results
+    val sumOfAllRaces = totalResults.sum()
+	val results = Results( totalResults.sorted() )   // intitialize the class Results with a sorted list of race results
 
-        println("List of race results unprocessed: $totalResults")
+    println("List of race results unprocessed: $totalResults")
 	println("Sum of all races: $sumOfAllRaces")
 	println("Nmbr of race discards: $nmbrOfDiscards")
 	println("Sorted list of race results: ${totalResults.sorted()}")
 	
-
-	results.resultRefinement { x: List<Int> ->
+	results.resultRefinement { x: List<Int> ->     // a trailing lambda, since only one parameter I can skip the parenthesis
 	  val lastIndex = x.count()-1
 	  var sumOfDiscards = 0
-	  for (i in 0..nmbrOfDiscards-1) {   // loop through the last race results in the sorted list, as many as race discards
-            sumOfDiscards = sumOfDiscards + x[lastIndex - i]
+	  for (i in 0..nmbrOfDiscards-1) {    // loop through the last race results in the sorted list, as many as race discards
+            sumOfDiscards = sumOfDiscards + x[lastIndex - i]        // loop backwards, highest race scores are at the end
 	  }
 
 	sumOfAllRaces - sumOfDiscards   // it needs to return an Int
@@ -30,7 +27,7 @@ fun resultCalculation(totalResults: MutableList<Int>, nmbrOfDiscards: Int = 0) {
 
 
 fun main() {
-	val totalResults = mutableListOf(12,13,7,4,3,5,6,8,15,17,9)  // this is just to test the lambdas
+	val totalResults = mutableListOf(12,13,7,4,3,5,6,8,15,17,9)     // this is just to test the lambdas
 	val nmbrOfDiscards = 3
 	resultCalculation( totalResults, nmbrOfDiscards)
 }
