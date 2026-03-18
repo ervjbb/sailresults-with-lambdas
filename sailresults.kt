c45e0d96158712e46392c4898c2bd402bcfcba24
Johan Baerring <45194960+ervjbb@users.noreply.github.com>
2026-03-18 10:00:57 +0100

    Changing to float calculations

--- sailresults.kt
+++ sailresults.kt
@@ -1,6 +1,6 @@
 class Results(val racePlacements: List<Int>) {
 
-fun scoreCalculation ( resultPolish: ( List<Int> ) -> Int ) {       // this is a lambda funtion
+fun scoreCalculation ( resultPolish: ( List<Int> ) -> Float ) {       // this is a lambda funtion
      val finalResult = resultPolish(racePlacements)             // it has no knowledge of discards nor scoring system
      println("Final result: $finalResult") 
 
@@ -11,7 +11,7 @@ fun scoreCalculation ( resultPolish: ( List<Int> ) -> Int ) {       // this is a
  
   }
  
-fun lowPointScoring(racePlacements: MutableList<Int>, nmbrOfDiscards: Int = 0) {   // default nmbr of discards = 0
+fun lowPointScoring(racePlacements: List<Int>, nmbrOfDiscards: Int = 0) {   // default nmbr of discards = 0
     println("lowPointScoring")
 	
     val sumOfAllRaces = racePlacements.sum()
@@ -19,37 +19,37 @@ fun lowPointScoring(racePlacements: MutableList<Int>, nmbrOfDiscards: Int = 0) {
 
 	results.scoreCalculation { x: List<Int> ->
 	  val lastIndex = x.count()-1
-	  var sumOfDiscards = 0
+	  var sumOfDiscards = 0f
 	  for (i in 0..nmbrOfDiscards-1) {   // loop through the last race results in the sorted list, as many as race discards
             sumOfDiscards = sumOfDiscards + x[lastIndex - i]
 	  }
-
-	sumOfAllRaces - sumOfDiscards   // it needs to return an Int
+	sumOfAllRaces - sumOfDiscards
 	} 
-
 }
 
-fun bonusPointScoring(racePlacements: MutableList<Int>, nmbrOfDiscards: Int = 0) {
+fun bonusPointScoring(racePlacements: List<Int>, nmbrOfDiscards: Int = 0) {
 	println("bonusPointScoring")
 
     val results = Results(racePlacements.sorted() )
-	var sum = 0
+	var sum = 0f
 
 	results.scoreCalculation { x: List<Int> ->
           for (item in x) {
 		  when (item) {                           // transform race placement to a bonus point
-			  1 -> sum = sum+0                   // a simplification made here: third place should have 5.7
-			  2 -> sum = sum+3                   // and sixth 11.7
-			  3 -> sum = sum+6                   // Staying with a List of Ints simplifies this
+			  1 -> sum = sum+0
+			  2 -> sum = sum+3
+			  3 -> sum = sum+5.7f
 			  4 -> sum = sum+8
 			  5 -> sum = sum+10
-			  6 -> sum = sum+12
+			  6 -> sum = sum+11.7f
 			  7 -> sum = sum+13
 		      else -> sum = sum+item+6
 		  }
-	
+	  }
+
+	  println("Sum: $sum")
       val lastIndex = x.count()-1
-	  var sumOfDiscards = 0
+	  var sumOfDiscards = 0f
 	  for (i in 0..nmbrOfDiscards-1) {   // loop through the last race results in the sorted list, as many as race discards
           	if ( x[lastIndex - i] > 7 ) sumOfDiscards = sumOfDiscards + x[lastIndex - i] + 6
 		    else if ( x[lastIndex - i] == 2) sumOfDiscards = sumOfDiscards + 3 
@@ -59,16 +59,13 @@ fun bonusPointScoring(racePlacements: MutableList<Int>, nmbrOfDiscards: Int = 0)
             else if ( x[lastIndex - i] == 6) sumOfDiscards = sumOfDiscards + 12
             else if ( x[lastIndex - i] == 7) sumOfDiscards = sumOfDiscards + 13
 	}
-
 	sum - sumOfDiscards
 	} 
 }
 
-
-
 fun main() {
-	val racePlacements = mutableListOf(12,13,7,4,3,5,6,8,15,17,9)  // this is just to test the lambdas
-	val nmbrOfDiscards = 3
+	val racePlacements = listOf(12,13,7,4,3,5,6,8,15,17,9)  // this is just to test the lambdas
+	val nmbrOfDiscards = 2
 
 	println("List of race results unprocessed: $racePlacements")
 	println("Sum of all races: ${racePlacements.sum()}")
